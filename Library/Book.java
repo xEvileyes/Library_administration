181062ec52f25d45483f2a0850f372a4de20be3f
xEvileyes <marvin.ziemann@web.de>
2024-08-15 19:01:40 +0200

    First Commit

---
+++ Library/Book.java
@@ -0,0 +1,38 @@
+public class Book {
+
+    private String title;
+	private String author;
+	private int pages;
+	final int LONGBOOK = 300;
+	
+	public Book(String title, String author, int pages) {			
+		this.title = title;
+		this.author = author;
+		this.pages = pages;			
+	}
+	
+	public void displayDetails() {		
+		System.out.println("Das Buch heißt "+title+" und wurde von "+author+" geschrieben. Das Buch hat "+pages+" Seiten.");
+	}
+	
+	public void displayTitle() {		
+		System.out.println(title);
+	}
+		
+	public void isLongBook() {		
+		if (pages > LONGBOOK) {
+			System.out.println("Das Buch zählt zu den langen Büchern, denn es hat mehr als "+LONGBOOK+" Seiten." );
+		}
+		else {
+			System.out.println("Das Buch zählt nicht zu den langen Büchern, denn es hat weniger als "+LONGBOOK+" Seiten.");
+		}
+	}
+	
+	public String getTitle() {
+		return title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+}
